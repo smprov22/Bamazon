9f67e5db79758ac22acbbd64b4af4fee572a177b
smprov22 <smprovence22@gmail.com>
2019-06-02 14:07:54 -0700

    initial prompt and basic functions created

--- bamazonManager.js
+++ bamazonManager.js
@@ -10,3 +10,45 @@ var connection = mysql.createConnection({
   password: "root",
   database: "bamazon_db"
 });
+
+function options() {
+    inquirer
+        .prompt([
+          {
+            name: "actions",
+            type: "rawlist",
+            message: "Please choose from the following actions.",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+          },
+        ])
+        .then(function(answer) {
+            if (answer.actions === "View Products for Sale") {
+                viewProducts();
+            } else if (answer.actions === "View Low Inventory") {
+                lowInventory();
+            } else if (answer.actions === "Add to Inventory") {
+                addInventory();
+            } else if (answer.actions === "Add New Product") {
+                addProduct();
+            }
+        })
+}
+
+function viewProducts() {
+    console.log("show products");
+}
+
+function lowInventory() {
+    console.log("show low inventory");
+}
+
+function addInventory() {
+    console.log("add to inventory");
+}
+
+function addProduct() {
+    console.log("add products to inventory")
+}
+
+
+options();
