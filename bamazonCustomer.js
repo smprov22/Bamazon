52d96429a1ac0b6413094fd1010157304eaf2a30
smprov22 <smprovence22@gmail.com>
2019-06-01 21:53:18 -0700

    initial files and beginning code

---
+++ bamazonCustomer.js
@@ -0,0 +1,38 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazon_db"
+});
+
+// connect to the mysql server and sql database
+connection.query("SELECT id, product_name, price FROM products", function(err, results) {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].product_name);
+            }
+            return choiceArray;
+          },
+          message: "Please enter the ID of the product you would like to buy?"
+        },
+      ])
+    })
